4184c99b35918da972251941defc0cc297f8491c
吴建鹏 <jianpeng.wu@geely.com>
2018-11-05 10:45:43 +0800

    feat: 字段main module得自动从package.json中取；

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,15 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import { main, module } from './package.json';
 
 export default {
   input: 'src/index.js',
   output: [{
-    file: 'dist/lib.cjs.js',
+    file: main,
     format: 'cjs'
   }, {
-    file: 'dist/lib.es.js',
+      file: module,
     format: 'es'
   }],
   plugins: [
