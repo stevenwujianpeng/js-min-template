23657fc1ca5a83781a6bcbc438c7e71985451e4c
吴建鹏 <jianpeng.wu@geely.com>
2018-11-09 22:00:50 +0800

    feat: 修改了babel env的配置环境

--- babel.config.js
+++ babel.config.js
@@ -6,8 +6,11 @@ module.exports = function (api) { // eslint-disable-line
 
   if (env === 'production') {
     presets[0].push({
-      "targets": "last 2 version",
-      "modules": false
+      "targets": {
+        "browsers": "> 0.25%, not dead",
+        "node": "6.8"
+      }, // 配置目标环境
+      "modules": false // 阻止 babel 将模块转成commonjs
     });
     plugins.push(["@babel/plugin-transform-runtime"]);
     // plugins.push(["@babel/plugin-transform-runtime", { "corejs": 2 }]); 当需要依赖ES6中的 扩展内置的对象或者方法的时候开启这个配置项
--- test/index.test.js
+++ test/index.test.js
@@ -6,37 +6,37 @@ var assert = require('assert');
 
 describe('fefeType', function () {
   it('undefined', function () {
-    assert.equal(feType(undefined),'undefined');
+    assert.equal(feType(undefined), 'undefined');
   });
   it('null', function () {
-    assert.equal(feType(null),'null');
+    assert.equal(feType(null), 'null');
   });
   it('number', function () {
-    assert.equal(feType(10),'number');
+    assert.equal(feType(10), 'number');
   });
   it('boolean', function () {
-    assert.equal(feType(true),'boolean');
-    assert.equal(feType(false),'boolean');
+    assert.equal(feType(true), 'boolean');
+    assert.equal(feType(false), 'boolean');
   });
   it('string', function () {
-    assert.equal(feType('test'),'string');
+    assert.equal(feType('test'), 'string');
   });
   it('Array', function () {
-    assert.equal(feType([1, 2]),'Array');
+    assert.equal(feType([1, 2]), 'Array');
   });
 
   it('Object', function () {
-    assert.equal(feType({name: 'Object'}),'Object');
+    assert.equal(feType({ name: 'Object' }), 'Object');
   });
   it('Date', function () {
-    assert.equal(feType(new Date()),'Date');
+    assert.equal(feType(new Date()), 'Date');
   });
   it('RegExp', function () {
-    assert.equal(feType(new RegExp()),'RegExp');
+    assert.equal(feType(new RegExp()), 'RegExp');
   });
   it('自定义的构造函数', function () {
     function CustomfeType() {
-  };
-    assert.equal(feType(new CustomfeType()),'CustomfeType');
+    }
+    assert.equal(feType(new CustomfeType()), 'CustomfeType');
   });
 })
